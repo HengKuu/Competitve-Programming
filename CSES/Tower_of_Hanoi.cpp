5f958c413b5e2d3e72cf51dd0121fb33403f4307
HengKuu <natasha081600@gmail.com>
2023-05-16 23:51:53 +0800

    testing

---
+++ CSES/Tower_of_Hanoi.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+
+}
+
+
+int main()
+{
+    cin.tie(nullptr); 
+    // can’t mix C-style C++-style IO
+    ios_base::sync_with_stdio(false);
+
+    solve();
+
+    return 0;
+}
